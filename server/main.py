cbf18222264cc59ddfc2cd6af2edfd28d62e0915
Mangesh <mangeshgosavi129@gmail.com>
2025-11-26 02:37:50 +0530

    server

--- server/main.py
+++ server/main.py
@@ -16,7 +16,7 @@ import os
 from dotenv import load_dotenv
 import logging
 # Load environment variables
-load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
 
 # =========================================================
 # DATABASE SETUP
